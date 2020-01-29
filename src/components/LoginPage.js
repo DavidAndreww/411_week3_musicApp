aff574c763eddeafb6b5fbc425491ee508185246
David Bille <d_bille@yahoo.com>
2020-01-28 18:10:20 -0600

    working on cards

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 
-const LoginPage = ( { onLoginChange } ) => {
+const LoginPage = ({ onLoginChange }) => {
   return (
       <form className="login-page" onSubmit={() => onLoginChange()}>
       <label>Username:</label>
