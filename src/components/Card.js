8ddd6df2aa2cd37e9324894072dece805eb3c2e1
David Bille <d_bille@yahoo.com>
2020-01-29 22:11:35 -0600

    cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import CardContent from "./CardContent";
-import CardActions from "./CardActions";
 
 const Card = ({ title, content, type, handler }) => {
   return (
     <Paper className="card" elevation={3}>
       <CardContent title={title} content={content} />
-      <CardActions type={type} handler={handler} />
     </Paper>
   );
 };
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "./Card";
 import Notifications from "./Notifications";
+import CardSwitch from "./CardSwitch";
+import CardSelect from "./CardSelect";
+import CardSlider from "./CardSlider";
 
 class Dashboard extends React.Component {
   state = {
-    notifications: [],
+    notifications: []
   };
 
-  handleCardActionChange = (message) => {
+  handleCardActionChange = message => {
     let notifications = this.state.notifications;
     this.setState({
       notifications: [...notifications, message]
@@ -28,24 +31,15 @@ class Dashboard extends React.Component {
     return (
       <div className="dashboard">
         <div className="card-container">
-          <Card
-            title={onlineCardTitle}
-            content={onlineCardContent}
-            type="switch"
-            handler={this.handleCardActionChange}
-          />
-          <Card
-            title={volumeCardTitle}
-            content={volumeCardContent}
-            type="slider"
-            handler={this.handleCardActionChangeitit}
-          />
-          <Card
-            title={soundCardTitle}
-            content={soundCardContent}
-            type="select"
-            handler={this.handleCardActionChange}
-          />
+          <Card title={onlineCardTitle} content={onlineCardContent}>
+            <CardSwitch handler={this.handleCardActionChange} />
+          </Card>
+          <Card title={volumeCardTitle} content={volumeCardContent}>
+            <CardSlider handler={this.handleCardActionChange} />
+          </Card>
+          <Card title={soundCardTitle} content={soundCardContent}>
+            <CardSelect handler={this.handleCardActionChange} />
+          </Card>
         </div>
         <Notifications alerts={this.state.notifications} />
       </div>
