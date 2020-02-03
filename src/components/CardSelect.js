9a0ac1926c019d264339e3a564248ef8bf906038
David Bille <d_bille@yahoo.com>
2020-02-02 20:11:07 -0600

    single source of truth I think completed properly for CardSelect component

--- src/components/CardSelect.js
+++ src/components/CardSelect.js
@@ -4,24 +4,30 @@ import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 
-const CardSelect = ({ handleCardActionChange }) => {
-  const handleSelectChange = e => {
+class CardSelect extends React.Component {
+  state= {
+    quality: ''
+  }
+   handleSelectChange = e => {
     let quality = e.target.value;
     if (quality === "low") {
-      handleCardActionChange("Sound quality has been degraded.");
+      this.props.handleCardActionChange("Sound quality has been degraded.");
     }
+    this.setState({quality})
   };
 
-  return (
-    <FormControl className="select">
-      <InputLabel>Quality</InputLabel>
-      <Select id="soundQuality" onChange={handleSelectChange}>
-        <MenuItem value="low">Low</MenuItem>
-        <MenuItem value="normal">Normal</MenuItem>
-        <MenuItem value="high">High</MenuItem>
-      </Select>
-    </FormControl>
-  );
+  render() {
+    return (
+      <FormControl className="select">
+        <InputLabel>Quality</InputLabel>
+        <Select id="soundQuality" onChange={this.handleSelectChange}>
+          <MenuItem value="low">Low</MenuItem>
+          <MenuItem value="normal">Normal</MenuItem>
+          <MenuItem value="high">High</MenuItem>
+        </Select>
+      </FormControl>
+    );
+  }
 };
 
 export default CardSelect;
